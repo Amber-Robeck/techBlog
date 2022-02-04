6ec8698dd8d97e0279c75de67f828c6ca0059073
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-03 20:10:54 -0600

    added a couple routes for home, login and signup, added navbar with links to main.handlebars and updated login handlebars and sign-up handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -7,16 +7,22 @@ const api = require('./api');
 // router.use('/dashboard', dashboardRoute);
 // router.use('/api', api);
 
-
-// router.use('*', )
-
-router.get('*', function (req, res) {
-    res.render('login')
+router.get('/', (req, res) => {
+    res.render('all-posts')
 })
 
+// router.use('*', )
+router.get('/adduser', (req, res) => {
+    res.render('signup')
+})
 router.post('/adduser', (req, res) => {
     console.log(req.body);
 
     res.redirect('/');
 });
+
+router.get('*', function (req, res) {
+    res.render('login')
+
+})
 module.exports = router;
