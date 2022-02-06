e2b682eab06904774aa3e71063848a6696bbc867
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-05 23:45:01 -0600

    added tag for if there are not any posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,32 +1,3 @@
-// const router = require('express').Router();
-// const { Post } = require('../models');
-// const withAuth = require('../utils/auth.js');
-
-
-// // /dashboard endpoint
-
-// //removed with auth for testing
-// router.get('/', async (req, res) => {
-//     try {
-//         res.render('all-posts-admin', { layout: 'dashboard' })
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
-
-// // /dashboard/new endpoint
-// // Add a new post
-// router.get('/new', async (req, res) => {
-//     try {
-//         res.status(200).render('new-post', { layout: 'dashboard' });
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
-
-
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
